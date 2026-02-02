67d5055c83b1a03e7825265b080ffec6bebabbb3
Federico Altava <fedealtava1992@gmail.com>
2026-02-02 15:07:53 +0100

    declarations css

---
+++ src/declarations.d.ts
@@ -0,0 +1,9 @@
+// src/declarations.d.ts
+declare module "*.css" {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.svg";
